d579dbcdddc958a85fbd5d955ce25f46713059e1
Oighty <oighty6@gmail.com>
2025-02-03 16:32:45 -0600

    chore: clean up

--- programs/common/src/constants.rs
+++ programs/common/src/constants.rs
@@ -7,6 +7,5 @@ pub const ANCHOR_DISCRIMINATOR_SIZE: usize = 8;
 
 pub const ADMIN: Pubkey = pubkey!("8cR6wkZ4umQQnYcxBwS1wfvPwPXHkuW3wf3toxBEaiAP"); 
 pub const MINT: Pubkey = pubkey!("J4a2cb2G6QbSsAxNiaEQKrshnt6ijnrCnjzDcDdcAbbK");
-// pub const REGISTRAR: Pubkey = pubkey!("BsvrfNL85iCTtQoxWwnLGJECBzpzqJw1zyYyZYLU5kex");
 
 pub const ONE: u64 = 1_000_000; // 1e6
