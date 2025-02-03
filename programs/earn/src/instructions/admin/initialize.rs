43e49c49215ca999ee1389676cbac05436a4af59
Oighty <oighty6@gmail.com>
2025-02-03 15:23:33 -0600

    feat: add admin earn instructions

--- programs/earn/src/instructions/admin/initialize.rs
+++ programs/earn/src/instructions/admin/initialize.rs
@@ -1,9 +1,66 @@
+// earn/instructions/admin/initialitze.rs
+
+// external dependencies
 use anchor_lang::prelude::*;
 
-#[derive(Accounts)]
-pub struct Initialize {}
+// local dependencies
+use common::{
+    constants::{ANCHOR_DISCRIMINATOR_SIZE, ADMIN, ONE},
+};
+use crate::{
+    state::{Global, GLOBAL_SEED},
+}
 
-pub fn handler(ctx: Context<Initialize>) -> Result<()> {
-    msg!("Greetings from: {}", ctx.program_id);
+#[derive(Accounts)]
+pub struct Initialize {
+    #[account(
+        mut,
+        address = ADMIN,
+    )]
+    pub signer: Signer<'info>,
+
+    #[account(
+        init,
+        payer = signer,
+        space = ANCHOR_DISCRIMINATOR_SIZE + Global::INIT_SPACE,
+        seeds = [GLOBAL_SEED],
+        bump
+    )]
+    pub global_account: Account<'info, Global>,
+
+    pub system_program: Program<'info, System>,
+}
+
+pub fn handler(
+    ctx: Context<Initialize>,
+    earn_authority: Pubkey, 
+    initial_index: u64,
+    claim_cooldown: u64
+) -> Result<()> {
+
+    // Check that the initial index is at least 1
+    if initial_index < ONE {
+        return err!(EarnError::InvalidParam);
+    }
+
+    // Initialize the global account
+    let global = &mut ctx.accounts.global_account;
+    global.earn_authority = earn_authority;
+    global.index = initial_index;
+    
+    let current_timestamp = Clock::get()?.unix_timestamp;
+    global.timestamp = current_timestamp;
+
+    global.claim_cooldown = claim_cooldown;
+
+    // Set the claim status to complete so that a new index can be propagated to start the first claim
+    global.claim_complete = true;
+    
+    // We explicitly set these values to zero for clarity
+    global.rewards_per_token = 0;
+    global.max_supply = 0;
+    global.max_yield = 0;
+    global.distributed = 0;
+    
     Ok(())
 }
---
+++ programs/earn/src/instructions/admin/set_earn_authority.rs
@@ -0,0 +1,30 @@
+// earn/instructions/admin/set_earn_authority.rs
+
+// external dependencies
+use anchor_lang::prelude::*;
+
+// local dependencies
+use common::constants::ADMIN;
+use crate::state::{Global, GLOBAL_SEED};
+
+#[derive(Accounts)]
+pub struct SetEarnAuthority<'info> {
+    #[account(address = ADMIN)]
+    pub signer: Signer<'info>,
+
+    #[account(
+        mut,
+        seeds = [GLOBAL_SEED],
+        bump
+    )]
+    pub global_account: Account<'info, Global>,
+}
+
+pub fn handler(
+    ctx: Context<SetEarnAuthority>,
+    new_earn_authority: Pubkey
+) -> Result<()> {
+    ctx.accounts.global_account.earn_authority = new_earn_authority;
+    
+    Ok(())
+}
