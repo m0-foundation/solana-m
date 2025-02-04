c547e181973df6de61b57cfcded5ec650d5d5099
Oighty <oighty6@gmail.com>
2025-02-04 16:26:16 -0600

    feat: remove registrar earner

--- programs/common/src/constants.rs
+++ programs/common/src/constants.rs
@@ -7,5 +7,6 @@ pub const ANCHOR_DISCRIMINATOR_SIZE: usize = 8;
 
 pub const ADMIN: Pubkey = pubkey!("8cR6wkZ4umQQnYcxBwS1wfvPwPXHkuW3wf3toxBEaiAP"); 
 pub const MINT: Pubkey = pubkey!("J4a2cb2G6QbSsAxNiaEQKrshnt6ijnrCnjzDcDdcAbbK");
+pub const ZERO_ADDRESS: Pubkey = pubkey!("11111111111111111111111111111111");
 
 pub const ONE: u64 = 1_000_000; // 1e6
---
+++ programs/earn/src/instructions/open/remove_registrar_earner.rs
@@ -0,0 +1,65 @@
+// earn/instructions/open/remove_registrar_earner.rs
+
+// external dependencies
+use anchor_lang::prelude::*;
+
+// local dependencies
+use common::constants::{MINT, ZERO_ADDRESS};
+use crate::{
+    constants::REGISTRAR,
+    state::{Earner, EARNER_SEED}
+};
+use registrar::{
+    constants::EARNER_LIST,
+    views::is_in_list,
+};
+
+#[derive(Accounts)]
+#[instruction(user: Pubkey)]
+pub struct RemoveRegistrarEarner<'info> {
+    pub signer: Signer<'info>,
+
+    #[account(
+        token::mint = MINT,
+        token::authority = user,
+    )]
+    pub user_token_account: InterfaceAccount<'info, TokenAccount>,
+
+    #[account(
+        mut,
+        close = signer,
+        seeds = [EARNER_SEED, user_token_account.key().as_ref()],
+        bump
+    )]
+    pub earner_account: Account<'info, Earner>,
+
+    /// CHECK: we validate this account within the instruction
+    /// since we expect it to be an externally owned PDA
+    pub registrar_flag: UncheckedAccount<'info>,
+
+    pub system_program: Program<'info, System>,
+}
+
+pub fn handler(ctx: Context<RemoveRegistrarEarner>, user: Pubkey, flag_bump: u8) -> Result<()> {
+    // Check if the user is still on the earner's list on the registrar
+    // If so or if the check fails, return an error
+    if is_in_list(
+        &REGISTRAR, 
+        &ctx.accounts.registrar_flag.to_account_info(),
+        flag_bump, 
+        &EARNER_LIST, 
+        &user
+    )? {
+        return err!(EarnError::NotAuthorized);
+    }
+
+    // Check that the earner does not have an earn_manager, if so, return an error
+    if ctx.accounts.earner_account != ZERO_ADDRESS {
+        return err!(EarnError::NotAuthorized);
+    }
+
+    // Set the is earning flag on the earner account to false, even though it's being closed
+    ctx.accounts.earner_account.is_earning = false;
+
+    Ok(())
+}
