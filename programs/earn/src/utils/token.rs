d097dab49ece92dd7f3fdfa82e5be1cc5a1a2170
Oighty <oighty6@gmail.com>
2025-02-24 16:01:16 -0600

    fix: make mint CPI compatible with multisig mint authority

--- programs/earn/src/utils/token.rs
+++ programs/earn/src/utils/token.rs
@@ -3,17 +3,20 @@
 
 // external dependencies
 use anchor_lang::prelude::*;
-use anchor_spl::token_interface::{
-    transfer_checked,
-    Burn, 
-    burn, 
-    Mint, 
-    MintTo, 
-    mint_to, 
-    TokenAccount, 
-    TokenInterface, 
-    TransferChecked
+use anchor_spl::{
+    token_interface::{
+        transfer_checked,
+        Burn, 
+        burn, 
+        Mint,
+        // mint_to, 
+        TokenAccount, 
+        TokenInterface, 
+        TransferChecked
+    }, 
+    token_2022::spl_token_2022::instruction::mint_to_checked
 };
+use solana_program::program::invoke_signed;
 
 pub fn transfer_tokens_from_program<'info>(
     from: &InterfaceAccount<'info, TokenAccount>,
@@ -76,23 +79,42 @@ pub fn mint_tokens<'info>(
     to: &InterfaceAccount<'info, TokenAccount>,
     amount: &u64,
     mint: &InterfaceAccount<'info, Mint>,
-    authority: &AccountInfo<'info>,
-    authority_seeds: &[&[&[u8]]],
+    multisig_authority: &AccountInfo<'info>,
+    signer: &AccountInfo<'info>,
+    signer_seeds: &[&[&[u8]]],
     token_program: &Interface<'info, TokenInterface>,
 ) -> Result<()> {
     // Build the arguments for the mint instruction
-    let mint_options = MintTo {
-        mint: mint.to_account_info(),
-        to: to.to_account_info(),
-        authority: authority.clone(),
-    };
+    // let cpi_context = CpiContext::new_with_signer(
+    //     token_program.to_account_info(),
+    //     vec![
+    //         mint.to_account_info(),
+    //         to.to_account_info(),
+    //         multisig_authority.clone(),
+    //         signer.clone(),
+    //     ],
+    //     signer_seeds);
 
-    let cpi_context = CpiContext::new_with_signer(token_program.to_account_info(), mint_options, authority_seeds);
+    // // Call the mint instruction
+    // mint_to(
+    //     cpi_context,
+    //     *amount
+    // )?;
 
-    // Call the mint instruction
-    mint_to(
-        cpi_context,
+    let ix = mint_to_checked(
+        token_program.to_account_info().key,
+        mint.to_account_info().key,
+        to.to_account_info().key,
+        multisig_authority.key,
+        &[signer.key],
         *amount,
+        6u8,
+    )?;
+    
+    invoke_signed(
+        &ix,
+        &[mint.to_account_info(), to.to_account_info(), multisig_authority.clone(), signer.clone()],
+        signer_seeds
     )?;
 
     Ok(())
