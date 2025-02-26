1f7c9fd3846cd44eda7b934e13797f3c761ff5a0
Oighty <oighty6@gmail.com>
2025-02-26 12:36:24 -0600

    chore: update TODOs

--- programs/earn/src/instructions/portal/propagate_index.rs
+++ programs/earn/src/instructions/portal/propagate_index.rs
@@ -89,8 +89,7 @@ pub fn handler(
     // Some max yield can be leftover from the previous period if yield was not claimed for some users.
     // To get the max yield for the next claim cycle, we take the difference between the current max yield 
     // and what was distributed to get the leftover amount. Then, we add the new potential max yield to be
-    // sent out. TODO confirm this won't get too large over time due to some users not earning.
-    // Should this be set to max u64 if it will overflow?
+    // sent out. 
     ctx.accounts.global_account.max_yield = ctx.accounts.global_account.max_yield
         .checked_sub(ctx.accounts.global_account.distributed).unwrap() // can probably remove the checked sub since distributed can't be greater than max yield
         .checked_add(period_max).unwrap();
