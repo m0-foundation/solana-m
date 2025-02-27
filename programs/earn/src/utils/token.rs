61f966b72657b796d9dececf0897cbe34d2ea0fe
Kurtis Streutker <kurtisstreutker@gmail.com>
2025-02-27 16:07:57 -0500

    remove dead code

--- programs/earn/src/utils/token.rs
+++ programs/earn/src/utils/token.rs
@@ -4,62 +4,9 @@
 use anchor_lang::prelude::*;
 use anchor_spl::{
     token_2022::spl_token_2022,
-    token_interface::{transfer_checked, Mint, TokenAccount, TokenInterface, TransferChecked},
+    token_interface::{Mint, TokenAccount, TokenInterface},
 };
 use solana_program::program::invoke_signed;
-
-pub fn transfer_tokens_from_program<'info>(
-    from: &InterfaceAccount<'info, TokenAccount>,
-    to: &InterfaceAccount<'info, TokenAccount>,
-    amount: &u64,
-    mint: &InterfaceAccount<'info, Mint>,
-    authority: &AccountInfo<'info>,
-    authority_seeds: &[&[&[u8]]],
-    token_program: &Interface<'info, TokenInterface>,
-) -> Result<()> {
-    // Build the arguments for the transfer instruction
-    let transfer_options = TransferChecked {
-        from: from.to_account_info(),
-        to: to.to_account_info(),
-        mint: mint.to_account_info(),
-        authority: authority.clone(),
-    };
-    let cpi_context = CpiContext::new_with_signer(
-        token_program.to_account_info(),
-        transfer_options,
-        authority_seeds,
-    );
-
-    // Call the transfer instruction
-    transfer_checked(cpi_context, *amount, mint.decimals)?;
-
-    Ok(())
-}
-
-pub fn transfer_tokens<'info>(
-    from: &InterfaceAccount<'info, TokenAccount>,
-    to: &InterfaceAccount<'info, TokenAccount>,
-    amount: &u64,
-    mint: &InterfaceAccount<'info, Mint>,
-    authority: &AccountInfo<'info>,
-    token_program: &Interface<'info, TokenInterface>,
-) -> Result<()> {
-    // Build the arguments for the transfer instruction
-    let transfer_options = TransferChecked {
-        from: from.to_account_info(),
-        to: to.to_account_info(),
-        mint: mint.to_account_info(),
-        authority: authority.clone(),
-    };
-    let cpi_context = CpiContext::new(token_program.to_account_info(), transfer_options);
-
-    // Call the transfer instruction
-    transfer_checked(cpi_context, *amount, mint.decimals)?;
-
-    Ok(())
-}
-
-// Convenience functions to mint and burn tokens from a program using a PDA signer
 
 pub fn mint_tokens<'info>(
     to: &InterfaceAccount<'info, TokenAccount>,
