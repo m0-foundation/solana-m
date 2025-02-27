c896ae95d28ba9a63e882b5c40613bc54f376899
Kurtis Streutker <kurtisstreutker@gmail.com>
2025-02-27 13:37:10 -0500

    export on feature

--- programs/earn/src/instructions/earn_authority/mod.rs
+++ programs/earn/src/instructions/earn_authority/mod.rs
@@ -5,7 +5,12 @@ pub mod complete_claims;
 
 pub use claim_for::ClaimFor;
 pub(crate) use claim_for::__client_accounts_claim_for;
-pub(crate) use claim_for::__cpi_client_accounts_claim_for;
 pub use complete_claims::CompleteClaims;
 pub(crate) use complete_claims::__client_accounts_complete_claims;
-pub(crate) use complete_claims::__cpi_client_accounts_complete_claims;
+
+cfg_if::cfg_if! {
+    if #[cfg(feature = "cpi")] {
+        pub(crate) use claim_for::__cpi_client_accounts_claim_for;
+        pub(crate) use complete_claims::__cpi_client_accounts_complete_claims;
+    }
+}
