b677b3b5964c85dac1b7fe9f171edc40daa207fb
Kurtis Streutker <kurtisstreutker@gmail.com>
2025-03-04 10:32:29 -0500

    use token example

--- substreams/src/pb/mod.rs
+++ substreams/src/pb/mod.rs
@@ -1,31 +1,10 @@
 // @generated
-pub mod sf {
-    pub mod solana {
-        pub mod r#type {
-            // @@protoc_insertion_point(attribute:sf.solana.type.v1)
-            pub mod v1 {
-                include!("sf.solana.type.v1.rs");
-                // @@protoc_insertion_point(sf.solana.type.v1)
-            }
+pub mod solana_token_tracker {
+    pub mod types {
+        // @@protoc_insertion_point(attribute:solana_token_tracker.types.v1)
+        pub mod v1 {
+            include!("solana_token_tracker.types.v1.rs");
+            // @@protoc_insertion_point(solana_token_tracker.types.v1)
         }
-    }
-    // @@protoc_insertion_point(attribute:sf.substreams)
-    pub mod substreams {
-        include!("sf.substreams.rs");
-        // @@protoc_insertion_point(sf.substreams)
-        pub mod solana {
-            // @@protoc_insertion_point(attribute:sf.substreams.solana.v1)
-            pub mod v1 {
-                include!("sf.substreams.solana.v1.rs");
-                // @@protoc_insertion_point(sf.substreams.solana.v1)
-            }
-        }
-    }
-}
-pub mod token {
-    // @@protoc_insertion_point(attribute:token.v1)
-    pub mod v1 {
-        include!("token.v1.rs");
-        // @@protoc_insertion_point(token.v1)
     }
 }
