0ffc37ca6ffa91f5548e17c425b70a4e00e584bf
Kurtis Streutker <kurtisstreutker@gmail.com>
2025-03-04 10:02:34 -0500

    Revert "small updates"
    This reverts commit 2b90001683b17ae43e67200b5a1e5ed473acdce4.

--- substreams/src/lib.rs
+++ substreams/src/lib.rs
@@ -1,18 +1,11 @@
 mod pb;
-
-use pb::token::v1::{TokenTransactionEvent, TokenTransactionEvents};
-use substreams_solana::pb::sf::solana::r#type::v1::Transaction;
-use substreams_solana_utils as utils;
+use pb::{sf::substreams::solana::v1::Transactions, token::v1::TokenTransactionData};
 
 #[substreams::handlers::map]
-fn map_token_transaction_data(transactions: Transaction) -> TokenTransactionEvents {
-    let mut events = TokenTransactionEvents::default();
+fn map_token_transaction_data(transactions: Transactions) -> TokenTransactionData {
+    // TODO: Modify this code to get the data that you need from the transactions.
 
-    for transaction in transactions.transactions.iter() {
-        events.transactions.push(TokenTransactionEvent {
-            signature: utils::transaction::get_signature(transaction),
-        });
-    }
-
-    events
+    let mut my_data = TokenTransactionData::default();
+    my_data.transactions = transactions.transactions;
+    my_data
 }
