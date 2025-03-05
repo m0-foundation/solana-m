a8ca3740ea1a22fd18fb7b371f0a0a1a422e301a
Kurtis Streutker <kurtisstreutker@gmail.com>
2025-03-05 08:42:37 -0500

    align versions

--- substreams/src/lib.rs
+++ substreams/src/lib.rs
@@ -1,16 +1,16 @@
 use events::Transactions;
 use pb::transfers::v1::{TokenTransaction, TokenTransactions};
+use substreams_solana_utils::transaction::get_signature;
 
 mod events;
 mod pb;
-mod utils;
 
 #[substreams::handlers::map]
 fn map_my_data(transactions: Transactions) -> TokenTransactions {
     let mut events = TokenTransactions::default();
 
     for transaction in transactions.transactions {
-        let signature = utils::get_signature(&transaction);
+        let signature = get_signature(&transaction);
         events.transactions.push(TokenTransaction { signature });
     }
 
