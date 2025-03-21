0b7cb7339ab41d794a3c0f085b7987080227199b
Kurtis Streutker <kurtisstreutker@gmail.com>
2025-03-21 16:10:11 -0400

    register earner

--- cli/utils.ts
+++ cli/utils.ts
@@ -1,39 +1,90 @@
 import { Keypair, Connection, PublicKey } from "@solana/web3.js";
 import { SolanaNtt } from "@wormhole-foundation/sdk-solana-ntt";
-import { SolanaPlatform, SolanaSendSigner } from "@wormhole-foundation/sdk-solana";
+import {
+  SolanaPlatform,
+  SolanaSendSigner,
+} from "@wormhole-foundation/sdk-solana";
 import { Wormhole } from "@wormhole-foundation/sdk";
+import Web3 from "web3";
 
-const PORTAL = new PublicKey("mzp1q2j5Hr1QuLC3KFBCAUz5aUckT6qyuZKZ3WJnMmY")
+const PORTAL = new PublicKey("mzp1q2j5Hr1QuLC3KFBCAUz5aUckT6qyuZKZ3WJnMmY");
 
 export function keysFromEnv(keys: string[]) {
-    return keys.map((key) => Keypair.fromSecretKey(Buffer.from(JSON.parse(process.env[key]))));
+  return keys.map((key) =>
+    Keypair.fromSecretKey(Buffer.from(JSON.parse(process.env[key])))
+  );
 }
 
-export function NttManager(connection: Connection, owner: Keypair, mint: PublicKey) {
-    const signer = new SolanaSendSigner(connection, "Solana", owner, false, { min: 300_000 });
-    const sender = Wormhole.parseAddress("Solana", signer.address());
+export function NttManager(
+  connection: Connection,
+  owner: Keypair,
+  mint: PublicKey
+) {
+  const signer = new SolanaSendSigner(connection, "Solana", owner, false, {
+    min: 300_000,
+  });
+  const sender = Wormhole.parseAddress("Solana", signer.address());
 
-    const wormholeNetwork = process.env.NETWORK === "devnet" ? "Testnet" : "Mainnet";
-    const wh = new Wormhole(wormholeNetwork, [SolanaPlatform]);
-    const ctx = wh.getChain("Solana");
+  const wormholeNetwork =
+    process.env.NETWORK === "devnet" ? "Testnet" : "Mainnet";
+  const wh = new Wormhole(wormholeNetwork, [SolanaPlatform]);
+  const ctx = wh.getChain("Solana");
 
-    const ntt = new SolanaNtt(
-        wormholeNetwork,
-        "Solana",
-        connection,
-        {
-            ...ctx.config.contracts,
-            ntt: {
-                token: mint.toBase58(),
-                manager: PORTAL.toBase58(),
-                transceiver: {
-                    wormhole: PORTAL.toBase58(),
-                },
-                quoter: "Nqd6XqA8LbsCuG8MLWWuP865NV6jR1MbXeKxD4HLKDJ"
-            },
+  const ntt = new SolanaNtt(
+    wormholeNetwork,
+    "Solana",
+    connection,
+    {
+      ...ctx.config.contracts,
+      ntt: {
+        token: mint.toBase58(),
+        manager: PORTAL.toBase58(),
+        transceiver: {
+          wormhole: PORTAL.toBase58(),
         },
-        "3.0.0",
-    );
+        quoter: "Nqd6XqA8LbsCuG8MLWWuP865NV6jR1MbXeKxD4HLKDJ",
+      },
+    },
+    "3.0.0"
+  );
 
-    return { ctx, ntt, signer, sender }
+  return { ctx, ntt, signer, sender };
+}
+
+export function getMerkleContract() {
+  const web3 = new Web3(
+    new Web3.providers.HttpProvider(process.env.ETH_SEPOLIA_RPC)
+  );
+
+  return new web3.eth.Contract(
+    [
+      {
+        inputs: [
+          {
+            internalType: "bytes32",
+            name: "list",
+            type: "bytes32",
+          },
+        ],
+        name: "getList",
+        outputs: ["bytes32[]"],
+        stateMutability: "view",
+        type: "function",
+      },
+      {
+        inputs: [
+          {
+            internalType: "bytes32",
+            name: "list",
+            type: "bytes32",
+          },
+        ],
+        name: "getRoot",
+        outputs: ["bytes32"],
+        stateMutability: "view",
+        type: "function",
+      },
+    ],
+    "0x050258e4761650ad774b5090a5DA0e204348Eb48"
+  );
 }
