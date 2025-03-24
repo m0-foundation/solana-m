d127ed6e2d14b43b4202e35444c68a8942872536
Oighty <oighty6@gmail.com>
2025-03-24 13:41:26 -0500

    feat: wrap and unwrap extension

---
+++ programs/ext_earn/src/instructions/open/mod.rs
@@ -0,0 +1,16 @@
+// ext_earn/instructions/open/mod.rs
+
+pub mod wrap;
+pub mod unwrap;
+
+pub use wrap::Wrap;
+pub(crate) use wrap::__client_accounts_wrap;
+pub use unwrap::Unwrap;
+pub(crate) use unwrap::__client_accounts_unwrap;
+
+cfg_if::cfg_if! {
+    if #[cfg(feature = "cpi")] {
+        pub(crate) use wrap::__cpi_client_accounts_wrap;
+        pub(crate) use unwrap::__cpi_client_accounts_unwrap;
+    }
+}
