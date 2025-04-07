c5c1d5ac7b548ceff04a9f15b5f68346ed9b00ce
Jordan Hall <jordan.hall@m0.xyx>
2025-04-07 12:15:12 -0500

    feat: bot debug updates

--- services/index-bot/main.ts
+++ services/index-bot/main.ts
@@ -31,131 +31,138 @@ export async function indexCLI() {
     .option('-t, --threshold [SECONDS]', 'Staleness threshold in seconds', '86400')
     .option('-f, --force', 'Force push the index even if it is not stale', false)
     .option('--dry-run', 'Do not send transactions', false)
-    .action(async ({solanaRpc, evmRpc, evmPrivateKey, threshold, force, dryRun}) => {
-        const options: ParsedOptions = {
-            solanaClient: new Connection(solanaRpc),
-            evmClient: createWalletClient({
-                transport: http(evmRpc),
-                account: privateKeyToAccount(evmPrivateKey),
-            }),
-            threshold: BigInt(threshold),
-            force,
-            dryRun
-        };
-        
-        await pushIndex(options);
+    .action(async ({ solanaRpc, evmRpc, evmPrivateKey, threshold, force, dryRun }) => {
+      const options: ParsedOptions = {
+        solanaClient: new Connection(solanaRpc),
+        evmClient: createWalletClient({
+          transport: http(evmRpc),
+          account: privateKeyToAccount(evmPrivateKey),
+        }),
+        threshold: BigInt(threshold),
+        force,
+        dryRun,
+      };
+
+      await pushIndex(options);
     });
 }
 
 async function pushIndex(options: ParsedOptions) {
-    if (!options.force) {
-        const isStale = await isIndexStale(options);
-        if (!isStale) {
-            logger.info('Index is not stale, skipping push');
-            return;
-        }
-        logger.info('Index is stale, pushing updated index');
-    } else {
-        logger.info('Force pushing index');
+  if (!options.force) {
+    const isStale = await isIndexStale(options);
+    if (!isStale) {
+      logger.info('Index is not stale, skipping push');
+      return;
     }
+    logger.info('Index is stale, pushing updated index');
+  } else {
+    logger.info('Force pushing index');
+  }
 
-    await sendIndexUpdate(options);
+  await sendIndexUpdate(options);
 
-    if (options.dryRun) {
-        logger.info('Dry run complete, not sending transaction');
-    } else {
-        logger.info('Index pushed successfully');
-    }
+  if (options.dryRun) {
+    logger.info('Dry run complete, not sending transaction');
+  } else {
+    logger.info('Index pushed successfully');
+  }
 }
 
 async function isIndexStale(options: ParsedOptions) {
-    // Get the current index from the Solana program
-    const globalAccount = await options.solanaClient.getAccountInfo(GLOBAL_ACCOUNT);
-    if (!globalAccount) {
-        throw new Error('Global account not found');
-    }
+  // Get the current index from the Solana program
+  const globalAccount = await options.solanaClient.getAccountInfo(GLOBAL_ACCOUNT);
+  if (!globalAccount) {
+    throw new Error('Global account not found');
+  }
 
-    const lastUpdateTimestamp = globalAccount.data.readBigUInt64BE(32 * 3 + 8); // timestamp is after 3 pubkeys and the index
-    const currentTimestamp = BigInt(Date.now() / 1000); // current timestamp in seconds
+  const lastUpdateTimestamp = globalAccount.data.readBigUInt64BE(32 * 3 + 8); // timestamp is after 3 pubkeys and the index
+  const currentTimestamp = BigInt(Date.now() / 1000); // current timestamp in seconds
 
-    if (options.dryRun) {
-        logger.info('Last update timestamp', { lastUpdateTimestamp: lastUpdateTimestamp.toString() });
-        logger.info('Current timestamp', { currentTimestamp: currentTimestamp.toString() });
-        logger.info('Threshold', { options: options.threshold.toString() });
-        logger.info('Is index stale', { stale: currentTimestamp - lastUpdateTimestamp > options.threshold });
-    }
+  if (options.dryRun) {
+    logger.debug('Checking index staleness', {
+      lastUpdateTimestamp: lastUpdateTimestamp.toString(),
+      currentTimestamp: currentTimestamp.toString(),
+      threshold: options.threshold.toString(),
+      stale: currentTimestamp - lastUpdateTimestamp > options.threshold,
+    });
+  }
 
-    return currentTimestamp - lastUpdateTimestamp > options.threshold;
+  return currentTimestamp - lastUpdateTimestamp > options.threshold;
 }
 
 async function sendIndexUpdate(options: ParsedOptions) {
-    // function sendMTokenIndex(
-    //     uint16 destinationChainId_,
-    //     bytes32 refundAddress_
-    // ) external payable returns (bytes32 messageId_)
-    //
-    // function quoteDeliveryPrice(
-    //     uint16 recipientChain,
-    //     bytes memory transceiverInstructions
-    // ) public view returns (uint256[] memory, uint256) {
-    const abi = [
-        {
-            inputs: [
-                { internalType: 'uint16', name: 'destinationChainId_', type: 'uint16' },
-                { internalType: 'bytes32', name: 'refundAddress_', type: 'bytes32' },
-            ],
-            name: 'sendMTokenIndex',
-            outputs: [{ internalType: 'bytes32', name: 'messageId_', type: 'bytes32' }],
-            stateMutability: 'payable',
-            type: 'function',
-        },
-        {
-            inputs: [
-                { internalType: 'uint16', name: 'recipientChain', type: 'uint16' },
-                { internalType: 'bytes', name: 'transceiverInstructions', type: 'bytes' },
-            ],
-            name: 'quoteDeliveryPrice',
-            outputs: [
-                { internalType: 'uint256[]', name: '', type: 'uint256[]' },
-                { internalType: 'uint256', name: '', type: 'uint256' },
-            ],
-            stateMutability: 'view',
-            type: 'function',
-        }
-    ] as const;
+  // function sendMTokenIndex(
+  //     uint16 destinationChainId_,
+  //     bytes32 refundAddress_
+  // ) external payable returns (bytes32 messageId_)
+  //
+  // function quoteDeliveryPrice(
+  //     uint16 recipientChain,
+  //     bytes memory transceiverInstructions
+  // ) public view returns (uint256[] memory, uint256) {
+  const abi = [
+    {
+      inputs: [
+        { internalType: 'uint16', name: 'destinationChainId_', type: 'uint16' },
+        { internalType: 'bytes32', name: 'refundAddress_', type: 'bytes32' },
+      ],
+      name: 'sendMTokenIndex',
+      outputs: [{ internalType: 'bytes32', name: 'messageId_', type: 'bytes32' }],
+      stateMutability: 'payable',
+      type: 'function',
+    },
+    {
+      inputs: [
+        { internalType: 'uint16', name: 'recipientChain', type: 'uint16' },
+        { internalType: 'bytes', name: 'transceiverInstructions', type: 'bytes' },
+      ],
+      name: 'quoteDeliveryPrice',
+      outputs: [
+        { internalType: 'uint256[]', name: '', type: 'uint256[]' },
+        { internalType: 'uint256', name: '', type: 'uint256' },
+      ],
+      stateMutability: 'view',
+      type: 'function',
+    },
+  ] as const;
 
-    const portal = getContract({
-        address: HUB_PORTAL,
-        abi,
-        client: options.evmClient,
+  const portal = getContract({
+    address: HUB_PORTAL,
+    abi,
+    client: options.evmClient,
+  });
+
+  // Get bridge price quote
+  // TODO verify the bytes input to this is correct.
+  // bytes(1) was taken from the scripts in the m-portal repo
+  const [, quote] = await portal.read.quoteDeliveryPrice([
+    1,
+    ('0x' + Buffer.from([1]).toString('hex')) as `0x${string}`,
+  ]);
+
+  // Send index update transaction
+  const refundAddress = ('0x' + options.evmClient.account?.address.slice(2).padStart(64, '0')) as `0x${string}`;
+
+  const params = {
+    wormholeDestinationChainId: 1,
+    refundAddress,
+    ethereumAccount: options.evmClient.account?.address,
+    ethereumChain: options.evmClient.chain,
+    value: quote.toString(),
+  };
+  if (options.dryRun) {
+    logger.debug('Bridge transaction params: ', params);
+  } else {
+    const tx = await portal.write.sendMTokenIndex([1, refundAddress], {
+      account: options.evmClient.account ?? null,
+      chain: options.evmClient.chain ?? null,
+      value: quote,
     });
 
-    // Get bridge price quote
-    // TODO verify the bytes input to this is correct.
-    // bytes(1) was taken from the scripts in the m-portal repo
-    const [, quote] = await portal.read.quoteDeliveryPrice([1, ('0x' + Buffer.from([1]).toString('hex')) as `0x${string}`]);
-    if (options.dryRun) {
-        logger.info('Bridge price quote: ', quote.toString());
-    }
+    logger.info('Transaction sent: ', tx);
+  }
 
-    // Send index update transaction
-    const refundAddress = ('0x' + options.evmClient.account?.address.slice(2).padStart(64,'0')) as `0x${string}`;
-    if (options.dryRun) {
-        logger.info('Destination chain (wormhole ID): ', 1);
-        logger.info('Refund address: ', refundAddress);
-        logger.info('Ethereum account: ', options.evmClient.account?.address);
-        logger.info('Ethereum chain: ', options.evmClient.chain);
-    } else {
-        const tx = await portal.write.sendMTokenIndex([1, refundAddress], { 
-            account: options.evmClient.account ?? null,
-            chain: options.evmClient.chain ?? null,
-            value: quote,
-         });
-    
-        logger.info('Transaction sent: ', tx);
-    }
-
-
+  return params;
 }
 
 function configureLogger() {
