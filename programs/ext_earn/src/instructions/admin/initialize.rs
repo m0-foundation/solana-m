7749a633e8d991f938ff58cde7e651323ebf414b
Kurtis Streutker <kurtisstreutker@gmail.com>
2025-04-08 08:51:03 -0400

    check

--- programs/ext_earn/src/instructions/admin/initialize.rs
+++ programs/ext_earn/src/instructions/admin/initialize.rs
@@ -7,18 +7,11 @@ use anchor_spl::token_interface::{Mint, Token2022};
 // local dependencies
 use crate::{
     constants::ANCHOR_DISCRIMINATOR_SIZE,
-    state::{
-        ExtGlobal, EXT_GLOBAL_SEED,
-        M_VAULT_SEED,
-        MINT_AUTHORITY_SEED,
-    },
+    state::{ExtGlobal, EXT_GLOBAL_SEED, MINT_AUTHORITY_SEED, M_VAULT_SEED},
 };
 use earn::{
+    state::{Global as EarnGlobal, GLOBAL_SEED as EARN_GLOBAL_SEED},
     ID as EARN_PROGRAM,
-    state::{
-        Global as EarnGlobal,
-        GLOBAL_SEED as EARN_GLOBAL_SEED,
-    },
 };
 
 #[derive(Accounts)]
@@ -36,12 +29,13 @@ pub struct Initialize<'info> {
     pub global_account: Account<'info, ExtGlobal>,
 
     #[account(
-        token::token_program = token_2022
+        mint::token_program = token_2022
     )]
     pub m_mint: InterfaceAccount<'info, Mint>,
 
     #[account(
-        token::token_program = token_2022
+        mint::token_program = token_2022,
+        mint::decimals = m_mint.decimals,
     )]
     pub ext_mint: InterfaceAccount<'info, Mint>,
 
@@ -57,12 +51,10 @@ pub struct Initialize<'info> {
     pub system_program: Program<'info, System>,
 }
 
-pub fn handler(
-    ctx: Context<Initialize>,
-    earn_authority: Pubkey,
-) -> Result<()> {
+pub fn handler(ctx: Context<Initialize>, earn_authority: Pubkey) -> Result<()> {
     let m_vault_bump = Pubkey::find_program_address(&[M_VAULT_SEED], ctx.program_id).1;
-    let ext_mint_authority_bump = Pubkey::find_program_address(&[MINT_AUTHORITY_SEED], ctx.program_id).1;
+    let ext_mint_authority_bump =
+        Pubkey::find_program_address(&[MINT_AUTHORITY_SEED], ctx.program_id).1;
 
     ctx.accounts.global_account.set_inner(ExtGlobal {
         admin: ctx.accounts.admin.key(),
