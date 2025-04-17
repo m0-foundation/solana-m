ee2aa9a813491649c4c22fcadb966456f7b1151e
Jordan Hall <oighty6@gmail.com>
2025-04-17 11:27:11 -0500

    feat: allow wrap/unwrap to other user's token accounts

--- programs/ext_earn/src/instructions/open/wrap.rs
+++ programs/ext_earn/src/instructions/open/wrap.rs
@@ -1,14 +1,13 @@
 // ext_earn/instructions/open/wrap.rs
 
 use anchor_lang::prelude::*;
-use anchor_spl::token_interface::{Mint, TokenAccount, Token2022};
+use anchor_spl::token_interface::{Mint, Token2022, TokenAccount};
 
 use crate::{
     errors::ExtError,
     state::{
-        M_VAULT_SEED,
-        MINT_AUTHORITY_SEED,
         global::{ExtGlobal, EXT_GLOBAL_SEED},
+        MINT_AUTHORITY_SEED, M_VAULT_SEED,
     },
     utils::token::{mint_tokens, transfer_tokens},
 };
@@ -49,7 +48,7 @@ pub struct Wrap<'info> {
         token::mint = m_mint,
         token::authority = signer,
     )]
-    pub user_m_token_account: InterfaceAccount<'info, TokenAccount>,
+    pub from_m_token_account: InterfaceAccount<'info, TokenAccount>,
 
     #[account(
         mut,
@@ -62,9 +61,8 @@ pub struct Wrap<'info> {
     #[account(
         mut,
         token::mint = ext_mint,
-        token::authority = signer,
     )]
-    pub user_ext_token_account: InterfaceAccount<'info, TokenAccount>,
+    pub to_ext_token_account: InterfaceAccount<'info, TokenAccount>,
 
     pub token_2022: Program<'info, Token2022>,
 }
@@ -72,22 +70,25 @@ pub struct Wrap<'info> {
 pub fn handler(ctx: Context<Wrap>, amount: u64) -> Result<()> {
     // Transfer the amount of m tokens from the user to the m vault
     transfer_tokens(
-        &ctx.accounts.user_m_token_account, // from
-        &ctx.accounts.vault_m_token_account, // to
-        amount, // amount
-        &ctx.accounts.m_mint, // mint
+        &ctx.accounts.from_m_token_account,     // from
+        &ctx.accounts.vault_m_token_account,    // to
+        amount,                                 // amount
+        &ctx.accounts.m_mint,                   // mint
         &ctx.accounts.signer.to_account_info(), // authority
-        &ctx.accounts.token_2022, // token program
+        &ctx.accounts.token_2022,               // token program
     )?;
 
     // Mint the amount of ext tokens to the user
     mint_tokens(
-        &ctx.accounts.user_ext_token_account, // to
-        amount, // amount
-        &ctx.accounts.ext_mint, // mint
-        &ctx.accounts.ext_mint_authority, // authority
-        &[&[MINT_AUTHORITY_SEED, &[ctx.accounts.global_account.ext_mint_authority_bump]]], // authority seeds
-        &ctx.accounts.token_2022, // token program
+        &ctx.accounts.to_ext_token_account, // to
+        amount,                             // amount
+        &ctx.accounts.ext_mint,             // mint
+        &ctx.accounts.ext_mint_authority,   // authority
+        &[&[
+            MINT_AUTHORITY_SEED,
+            &[ctx.accounts.global_account.ext_mint_authority_bump],
+        ]], // authority seeds
+        &ctx.accounts.token_2022,           // token program
     )?;
 
     Ok(())
