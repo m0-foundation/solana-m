b300bc7b0eb258b4ce43818eafde7ce850b40776
Kurtis Streutker <kurtisstreutker@gmail.com>
2025-04-19 15:34:28 -0400

    update sdk

--- dashboard/src/services/sdk.ts
+++ dashboard/src/services/sdk.ts
@@ -2,7 +2,7 @@ import { createPublicClient, Earner, Graph, http } from '@m0-foundation/solana-m
 import { connection } from './rpc';
 import { PublicKey } from '@solana/web3.js';
 
-const evmClient = createPublicClient({ transport: http(process.env.ETH_RPC_URL ?? '') });
+const evmClient = createPublicClient({ transport: http(import.meta.env.VITE_EVM_RPC_URL ?? '') });
 const graphClient = new Graph(import.meta.env.VITE_GRAPH_KEY, import.meta.env.VITE_SUBGRAPH_URL.split('/').pop());
 
 export const getEarner = async (pubkey: PublicKey) => {
