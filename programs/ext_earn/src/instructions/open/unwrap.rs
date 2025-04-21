fc5d311bf04448f0be86c7a69d778d5cb824ffec
Jordan Hall <90539730+Oighty@users.noreply.github.com>
2025-04-21 09:52:55 -0500

    PROTO-129: feat: wrap/unwrap to third-party wallets (#74)
    * feat: allow wrap/unwrap to other user's token accounts
    
    * test: update wrap/unwrap tests

--- programs/ext_earn/src/instructions/open/unwrap.rs
+++ programs/ext_earn/src/instructions/open/unwrap.rs
@@ -1,13 +1,13 @@
 // ext_earn/instructions/open/unwrap.rs
 
 use anchor_lang::prelude::*;
-use anchor_spl::token_interface::{Mint, TokenAccount, Token2022};
+use anchor_spl::token_interface::{Mint, Token2022, TokenAccount};
 
 use crate::{
     errors::ExtError,
     state::{
-        M_VAULT_SEED,
         global::{ExtGlobal, EXT_GLOBAL_SEED},
+        M_VAULT_SEED,
     },
     utils::token::{burn_tokens, transfer_tokens_from_program},
 };
@@ -39,9 +39,8 @@ pub struct Unwrap<'info> {
     #[account(
         mut,
         token::mint = m_mint,
-        token::authority = signer,
     )]
-    pub user_m_token_account: InterfaceAccount<'info, TokenAccount>,
+    pub to_m_token_account: InterfaceAccount<'info, TokenAccount>,
 
     #[account(
         mut,
@@ -56,30 +55,30 @@ pub struct Unwrap<'info> {
         token::mint = ext_mint,
         token::authority = signer,
     )]
-    pub user_ext_token_account: InterfaceAccount<'info, TokenAccount>,
+    pub from_ext_token_account: InterfaceAccount<'info, TokenAccount>,
 
-    pub token_2022: Program<'info, Token2022>, 
+    pub token_2022: Program<'info, Token2022>,
 }
 
 pub fn handler(ctx: Context<Unwrap>, amount: u64) -> Result<()> {
     // Burn the amount of ext tokens from the user
     burn_tokens(
-        &ctx.accounts.user_ext_token_account, // from
-        amount, // amount
-        &ctx.accounts.ext_mint, // mint
+        &ctx.accounts.from_ext_token_account,   // from
+        amount,                                 // amount
+        &ctx.accounts.ext_mint,                 // mint
         &ctx.accounts.signer.to_account_info(), // authority
-        &ctx.accounts.token_2022, // token program
+        &ctx.accounts.token_2022,               // token program
     )?;
 
     // Transfer the amount of m tokens from the m vault to the user
     transfer_tokens_from_program(
         &ctx.accounts.vault_m_token_account, // from
-        &ctx.accounts.user_m_token_account, // to
-        amount, // amount
-        &ctx.accounts.m_mint, // mint
-        &ctx.accounts.m_vault, // authority
+        &ctx.accounts.to_m_token_account,    // to
+        amount,                              // amount
+        &ctx.accounts.m_mint,                // mint
+        &ctx.accounts.m_vault,               // authority
         &[&[M_VAULT_SEED, &[ctx.accounts.global_account.m_vault_bump]]], // authority seeds
-        &ctx.accounts.token_2022, // token program
+        &ctx.accounts.token_2022,            // token program
     )?;
 
     Ok(())
