e37b6ffc1e6fc3d0e850d400d315db4d5fe7aba8
Jordan Hall <oighty6@gmail.com>
2025-04-23 13:41:45 -0500

    feat: simulate clean-up

--- dashboard/src/components/simulate.tsx
+++ dashboard/src/components/simulate.tsx
@@ -5,22 +5,28 @@ import { Message } from '@solana/web3.js';
 export const Simulate = () => {
   const [typedMessage, setTypedMessage] = useState('');
   const [linkToTxn, setLinkToTxn] = useState('');
+  const [parseError, setParseError] = useState(false);
 
   const handleMessageChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     const value = e.target.value;
 
     if (value === '') {
       setLinkToTxn('');
+      setParseError(false);
     }
 
+    setTypedMessage(value);
+
     // allow json format
-    if (/^[\s\S]*$/.test(value)) {
-      setTypedMessage(value);
+    if (!/^[\s\S]*$/.test(value)) {
+      setParseError(true);
+      return;
     }
 
     // convert the typed message into a serialized solana transaction
     try {
       const parsedMessage = JSON.parse(value);
+      console.log(parsedMessage);
       const message = new Message(parsedMessage.message);
 
       const serializedMessage = message.serialize().toString('base64');
@@ -28,16 +34,17 @@ export const Simulate = () => {
       // escape the serialized string for the URL
       const escapedSerialized = encodeURIComponent(serializedMessage);
 
-      setLinkToTxn(
-        `https://solana.fm/inspector?message=${escapedSerialized}&signatures=%255B%25221111111111111111111111111111111111111111111111111111111111111111%2522%255D` +
-          NETWORK ===
-          'devnet'
-          ? '&cluster=devnet-alpha'
-          : '',
-      );
+      let url = `https://solana.fm/inspector?message=${escapedSerialized}&signatures=%255B%25221111111111111111111111111111111111111111111111111111111111111111%2522%255D`;
+      if (NETWORK === 'devnet') {
+        url += '&cluster=devnet-alpha';
+      }
+
+      setLinkToTxn(url);
+      setParseError(false);
     } catch (error) {
       console.error('Error parsing transaction:', error);
       setLinkToTxn('');
+      setParseError(true);
       return;
     }
   };
@@ -53,14 +60,19 @@ export const Simulate = () => {
           rows={10}
           className="w-full bg-off-blue py-3 px-4 pr-20 focus:outline-none"
         />
-        <a href={linkToTxn} target="_blank" rel="noopener noreferrer" className="text-blue-500 underline">
-          <button
-            disabled={linkToTxn === ''}
-            className={'text-white py-2 px-4 rounded mt-4' + (linkToTxn === '' ? ' bg-white-500' : ' bg-blue-500')}
-          >
-            <h2 className="text-lg font-semibold">Link to Simulate</h2>
-          </button>
-        </a>
+        {parseError ? (
+          <h2 className="text-lg font-semibold font-red-500">Invalid Input</h2>
+        ) : (
+          <a href={linkToTxn} target="_blank" rel="noopener noreferrer" className="text-blue-500 underline">
+            <button
+              disabled={linkToTxn === ''}
+              className={'text-white py-2 px-4 rounded mt-4' + (linkToTxn === '' ? ' bg-white-500' : ' bg-blue-500')}
+              // className="text-white py-2 px-4 rounded mt-4 bg-blue-500"
+            >
+              <h2 className="text-lg font-semibold">Link to Simulate</h2>
+            </button>
+          </a>
+        )}
       </div>
     </div>
   );
