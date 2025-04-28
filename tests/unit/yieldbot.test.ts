895e783698bf8f4f1041d4238e34aa67f5c12103
Kurtis Streutker <kurtisstreutker@gmail.com>
2025-04-28 09:48:40 -0400

    fix test

--- tests/unit/yieldbot.test.ts
+++ tests/unit/yieldbot.test.ts
@@ -117,7 +117,10 @@ function mockRequestData(earner: PublicKey) {
     ],
     [
       (body: any) =>
-        body.method === 'getAccountInfo' && body.params?.[0] === 'mzeroZRGCah3j5xEWp2Nih3GDejSBbH1rbHoxDg8By6', // mint
+        body.method === 'getAccountInfo' &&
+        ['mzeroZRGCah3j5xEWp2Nih3GDejSBbH1rbHoxDg8By6', 'mzeroXDoBpRVhnEXBra27qzAMdxgpWVY3DzQW7xMVJp'].includes(
+          body.params?.[0],
+        ), // mint
       {
         context,
         value: {
@@ -130,6 +133,24 @@ function mockRequestData(earner: PublicKey) {
           owner: 'TokenzQdBNbLqP5VEhdkAS6EPFLC1PHnBqCXEpPxuEb',
           rentEpoch: 18446744073709551615,
           space: 569,
+        },
+      },
+    ],
+    [
+      (body: any) =>
+        body.method === 'getAccountInfo' && body.params?.[0] === 'GQBavw2gpCdbZkkSWk9PkzNTDdBwCHUGNpeuuQ7mV9GA', // wM global
+      {
+        context,
+        value: {
+          data: [
+            'nT0aSBDxU4yz3HtcE1xihhozJWJpdvNsPnG5FAKFUFeJ7wZJIrxP9rPce1wTXGKGGjMlYml282w+cbkUAoVQV4nvBkkivE/2C4a+ZrwfmLR9IKO+YVpJBagluCaGTioPTJSEZ9M+5wkLhr5mv860wdfpJ7zE0BS+Dyhjq534X9phCFG2Tb0K5eRoMAbaMvJBTyQcLMmsnaDkH0FwZa+QwkrYCQghj/MVxrIB0uoAAAB3Ng9oAAAAAP/+/A==',
+            'base64',
+          ],
+          executable: false,
+          lamports: 2192400,
+          owner: 'wMXX1K1nca5W4pZr1piETe78gcAVVrEFi9f4g46uXko',
+          rentEpoch: 18446744073709551615,
+          space: 187,
         },
       },
     ],
