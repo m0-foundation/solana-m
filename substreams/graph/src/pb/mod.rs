4da2136ec1d936addb854d86c41f81829e7ec8d0
Kurtis Streutker <kurtisstreutker@gmail.com>
2025-04-30 09:35:08 -0400

    PROTO-145: substreams - mongo (#105)
    * move files
    
    * PROTO-145: implement substream + mongo db (#104)
    
    * db protos and parsing
    
    * conditional stream build
    
    * fixes
    
    * parsing events to db changes
    
    * schema and update image
    
    * fix image
    
    * correct platform
    
    * devnet and mainnet deploys

--- substreams/graph/src/pb/mod.rs
+++ substreams/graph/src/pb/mod.rs
@@ -1,4 +1,11 @@
 // @generated
+pub mod database {
+    // @@protoc_insertion_point(attribute:database.v1)
+    pub mod v1 {
+        include!("database.v1.rs");
+        // @@protoc_insertion_point(database.v1)
+    }
+}
 pub mod transfers {
     // @@protoc_insertion_point(attribute:transfers.v1)
     pub mod v1 {
