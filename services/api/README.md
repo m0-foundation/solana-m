35a09058c56183477393191c548a23b866339e84
Kurtis Streutker <kurtisstreutker@gmail.com>
2025-05-13 09:21:17 -0400

    claims endpoint

--- services/api/README.md
+++ services/api/README.md
@@ -4,7 +4,7 @@ The API is built using [Fern](https://github.com/fern-api/fern)
 
 > Fern is a toolkit that allows you to input your API Definition and output SDKs and API documentation. Fern is compatible with the OpenAPI specification (formerly Swagger).
 
-The `fern` directory contains the api definitions which fern uses to generated the code within `sdk` and `server`. To add or modify an endpoint, update or add the respective service definition file in `fern/definitions` and then run `make generate-api-code`.
+The `fern` directory contains the api definitions which fern uses to generated the code within `sdk` and `server`. To add or modify an endpoint, update or add the respective service definition file in `fern/definitions` and then run `make generate-api-code` (make sure to use the `--local` flag when generating code else it do it remotely and ask for fern credentials).
 
 ```
 .
--- services/api/server/src/events.ts
+++ services/api/server/src/events.ts
@@ -1,13 +1,17 @@
-import { Bridge } from '../generated/api';
+import { Bridge, Claim } from '../generated/api';
 import { EventsService } from '../generated/api/resources/events/service/EventsService';
 import { database } from './db';
 
+const parseLimitQuery = (reqQuery: { skip?: number; limit?: number }) => {
+  return { skip: reqQuery?.skip ?? 0, limit: Math.min(reqQuery?.limit ?? 100, 1000) };
+};
+
 export const events = new EventsService({
   bridges: async (req, res, next) => {
-    const { limit, skip } = req.query;
+    const { limit, skip } = parseLimitQuery(req.query);
 
     const coll = database.collection('bridge_events');
-    const cursor = coll.find({}, { skip: skip ?? 0, limit: Math.min(limit ?? 100, 1000) });
+    const cursor = coll.find({}, { limit, skip });
     const result = await cursor.toArray();
 
     res.send({
@@ -23,6 +27,35 @@ export const events = new EventsService({
           ts: bridge.transaction.block_time,
         };
         return bridgeEvent;
+      }),
+    });
+  },
+
+  claims: async (req, res, next) => {
+    const { limit, skip } = parseLimitQuery(req.query);
+    const { programId, tokenAccount } = req.params;
+
+    const cursor = database
+      .collection('claim_events')
+      .find(
+        { token_account: tokenAccount, program_id: programId },
+        { limit, skip, sort: { 'transaction.block_height': -1 } },
+      );
+
+    const result = await cursor.toArray();
+
+    res.send({
+      claims: result.map((claim) => {
+        const claimEvent: Claim = {
+          amount: claim.amount,
+          index: claim.index,
+          programId: claim.program_id,
+          tokenAccount: claim.token_account,
+          recipientTokenAccount: claim.recipient_token_account,
+          signature: claim.signature,
+          ts: claim.transaction.block_time,
+        };
+        return claimEvent;
       }),
     });
   },
