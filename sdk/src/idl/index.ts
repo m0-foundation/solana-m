0b64e78da7b5dc9e678b7f104a783e35d59408e5
Kurtis Streutker <kurtisstreutker@gmail.com>
2025-05-14 14:00:27 -0400

    sdk updates

--- sdk/src/idl/index.ts
+++ sdk/src/idl/index.ts
@@ -1,21 +1,18 @@
 import { AnchorProvider, Program, Wallet } from '@coral-xyz/anchor';
 import { Earn } from './earn';
-import { EXT_PROGRAM_ID, PROGRAM_ID } from '..';
 import { Connection, Keypair, PublicKey, Transaction, VersionedTransaction } from '@solana/web3.js';
 import { ExtEarn } from './ext_earn';
 const EARN_IDL = require('./earn.json');
 const EXT_EARN_IDL = require('./ext_earn.json');
 
 export function getProgram(connection: Connection): Program<Earn> {
-  const dummyKeypair = Keypair.generate();
   const provider = new AnchorProvider(connection, new DummyWallet(), { commitment: connection.commitment });
-  return new Program<Earn>(EARN_IDL, PROGRAM_ID, provider);
+  return new Program<Earn>(EARN_IDL, provider);
 }
 
 export function getExtProgram(connection: Connection): Program<ExtEarn> {
-  const dummyKeypair = Keypair.generate();
   const provider = new AnchorProvider(connection, new DummyWallet(), { commitment: connection.commitment });
-  return new Program<ExtEarn>(EXT_EARN_IDL, EXT_PROGRAM_ID, provider);
+  return new Program<ExtEarn>(EXT_EARN_IDL, provider);
 }
 
 class DummyWallet implements Wallet {
