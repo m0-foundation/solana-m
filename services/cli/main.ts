3540a5b8c9ae2a93ec53d7fe0d93fc2d5940d057
Kurtis Streutker <kurtisstreutker@gmail.com>
2025-05-28 15:20:16 -0400

    fix cli

--- services/cli/main.ts
+++ services/cli/main.ts
@@ -635,7 +635,7 @@ async function main() {
           addressesForTable.push(earner.pubkey, earner.data.userTokenAccount);
 
           // Check if there is an earn manager
-          if (earner.data.earnManager && !addressesForTable.find((a) => a.equals(earner.data.earnManager))) {
+          if (earner.data.earnManager && !addressesForTable.find((a) => a.equals(earner.data.earnManager!))) {
             addressesForTable.push(earner.data.earnManager);
           }
         }
