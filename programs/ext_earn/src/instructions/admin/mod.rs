62f52181caf08f0be87d690e205f5cebd170aef2
Kurtis Streutker <kurtisstreutker@gmail.com>
2025-06-04 13:04:57 -0400

    add authorities

--- programs/ext_earn/src/instructions/admin/mod.rs
+++ programs/ext_earn/src/instructions/admin/mod.rs
@@ -1,6 +1,7 @@
 // ext_earn/instructions/admin/mod.rs
 
 pub mod add_earn_manager;
+pub mod add_wrap_authority;
 pub mod initialize;
 pub mod remove_earn_manager;
 pub mod set_earn_authority;
@@ -8,6 +9,8 @@ pub mod set_m_mint;
 
 pub use add_earn_manager::AddEarnManager;
 pub(crate) use add_earn_manager::__client_accounts_add_earn_manager;
+pub use add_wrap_authority::AddWrapAuthority;
+pub(crate) use add_wrap_authority::__client_accounts_add_wrap_authority;
 pub use initialize::Initialize;
 pub(crate) use initialize::__client_accounts_initialize;
 pub use remove_earn_manager::RemoveEarnManager;
@@ -24,5 +27,6 @@ cfg_if::cfg_if! {
         pub(crate) use remove_earn_manager::__cpi_client_accounts_remove_earn_manager;
         pub(crate) use set_earn_authority::__cpi_client_accounts_set_earn_authority;
         pub(crate) use set_m_mint::__cpi_client_accounts_set_m_mint;
+        pub(crate) use add_wrap_authority::__cpi_client_accounts_add_wrap_authority;
     }
 }
